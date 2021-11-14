feec1df6ca664cf39e21875e5ea01f1b359f53ec
Matthew Ackman <matthewackman@Matthews-MacBook-Pro.local>
2021-11-13 20:52:01 -0700

    Cleaned up var names, modified plots

--- rmd.py
+++ rmd.py
@@ -3,6 +3,9 @@
 # -*- coding: utf-8 -*-
 """
 Robust multivariate outlier detection using Mahalanobis distance
+
+NOTE: This project is incomplete
+
 @author: mattackman
 """
 
@@ -24,7 +27,7 @@ from pandas.core.common import SettingWithCopyWarning
 warnings.filterwarnings("ignore")
 
 '''
-Mahanobis distance measures the distane between a point and a distribution
+Mahanobis distance measures the distance between a point and a distribution
 and can be thought of as multivariate euclidian distance. It is useful for 
 detecting observations in a panel which are outliers in several dimensions.
 '''
@@ -33,7 +36,7 @@ detecting observations in a panel which are outliers in several dimensions.
 def mahalanobis(x=None, robust=True):
     
     '''
-    Retuns an N*1 vector of mahalanobis distances for each obs in a matrix
+    Retuns an N*1 vector of mahalanobis distances for each row in a matrix.
     
     Params:
         mat {Pandas dataframe} - Input matrix, must have no NaNs
@@ -53,44 +56,50 @@ def mahalanobis(x=None, robust=True):
 The practical utility of using robust Mahalanobis distance for outlier 
 detection will be demonstrated by contaminating a generated 2D sample of
 data points with a much sparser cloud of points. The naive (ie conventional 
-ovariance matrix) will fail to reject outliers as the contaminant cloud 
+covariance matrix) will fail to reject outliers as the contaminant cloud 
 approaches the size or the original target cloud, whereas the robust MD
 will be more successful.
+
+Barring data visualization constraints, this process is scalable to n-dimenstions.
 '''
 
 ## STATIC VARS
 
-nObs = 5000  # number of observations
-nCol = 2  # number of columns
-shOut = 0.1  # share of observations which are outliers
-nOut = int(shOut * nObs)
+N_OBS = 5000  # number of observations
+N_COL = 2  # number of columns
+SH_OUT = 0.5  # share of observations which are outliers
+N_OUT = int(SH_OUT * N_OBS)  # number of outliers
+CMAP = 'viridis_r'  # matplotlib colourmap used for plots
 
-cov = np.eye(nCol)  # indentity matrix (nCol * nCol)
-cov[0,0] = 3  # covariance of general obs
-cov[0,1] = 2  # covariance of general obs
+## DEFINE COVARIANCES
 
+cov = np.eye(N_COL)  # indentity matrix (N_OBS * N_COL)
+cov[0,0] = 3  # manually set correlations
 
-covC = np.eye(nCol)  # covariance of contaminated sample
-covC[np.arange(1,nCol), np.arange(1,nCol)] = 3  # stdev of contaminated obs
+covC = np.eye(N_COL)  # covariance of contaminated sample
+covC[np.arange(1,N_COL), np.arange(1,N_COL)] = 10  # stdev of contaminated obs
 
-# RMD
+# CALCULATE AND DISPLAY MAHALANOBIS DISTANCE
 
-X = np.dot(np.random.randn(nObs, nCol), cov)
+X = np.dot(np.random.randn(N_OBS, N_COL), cov)
 
-
-X[-nOut:] = np.dot(np.random.randn(nOut, nCol), covC)
+X[-N_OUT:] = np.dot(np.random.randn(N_OUT, N_COL), covC)  # contaminate sample
 
 X = pd.DataFrame(X, columns=['Var1','Var2'])
 
-X['cont'] = (X.index >= (1-shOut)*nObs).astype(int)
+X['cont'] = (X.index >= (1-SH_OUT)*N_OBS).astype(int)
 
 X['RMD'] = mahalanobis(X[['Var1','Var2']], robust=True)
-X['Pval'] = np.sqrt(chi2.ppf(0.95, df=nCol))
+X['pVal'] = 1 - chi2.cdf(X.RMD, df=N_COL)
 
-fig, ax = plt.subplots(1,2)
+fig, ax = plt.subplots(1,2,figsize=(10,6))
 
-X.plot.scatter('Var1','Var2', c='cont', cmap='viridis', ax=ax[0])
-X.plot.scatter('Var1', 'Var2', c='Pval', cmap='viridis', ax=ax[1])
+X.plot.scatter('Var1','Var2', c='cont', cmap=CMAP, ax=ax[0],
+title='Originated vs contaminated observations', xlabel='', ylabel='')
+
+X.plot.scatter('Var1', 'Var2', c='RMD', cmap=CMAP, ax=ax[1],
+title='Robust mahalanobuis distance helps identify correct cluster',
+xlabel='',ylabel='', vmin=0, vmax=20)
 
 fig.tight_layout()
 
